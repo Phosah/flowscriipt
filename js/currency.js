eb4de6d7c7b0a3d7ba282cb6af420b06477fa308
Phosah <efosa.uyi@gmail.com>
2023-11-07 16:42:10 +0100

    input mask - currency function

--- js/currency.js
+++ js/currency.js
@@ -7,8 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
     input.addEventListener("input", function (event) {
       let value = event.target.value;
 
-      if (maskType === "decimal") {
-        const numericValue = value.replace(/[^\d.]/g, "");
+      if (maskType === "currency") {
+        let numericValue = value.replace(/[^\d.]/g, "");
+
+        if (numericValue === "" || numericValue === "$ ") {
+          this.value = "";
+          return;
+        }
+
+        if (numericValue.charAt(0) !== "$") numericValue = `$ ${numericValue}`;
 
         // Ensure only one decimal point is present
         const parts = numericValue.split(".");
@@ -16,8 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
           parts.pop();
         }
 
+        if (parts[1] && parts[1].length > 2) {
+          parts[1] = parts[1].substring(0, 2);
+        }
+
         this.value = parts.join(".");
       }
     });
   });
 });
+
+console.log("This script works");
