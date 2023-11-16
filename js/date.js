cb849c0489f40ce1738555e118a0691946ffb8ca
Phosah <efosa.uyi@gmail.com>
2023-11-16 19:02:17 +0100

    update to the product key input mask

--- js/date.js
+++ js/date.js
@@ -1,235 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const inputElements = document.querySelectorAll("[fs-input-mask]");
-
-//   inputElements.forEach(function (input) {
-//     const maskType = input.getAttribute("fs-input-mask");
-
-//     if (maskType === "date") {
-//       input.value = "__/__/____";
-
-//       input.addEventListener("keydown", function (event) {
-//         if (event.key === "Backspace" || event.key === "Delete") {
-//           event.preventDefault();
-
-//           let pos = input.selectionStart;
-//           if (pos !== 0) {
-//             pos--;
-//             if (input.value[pos - 1] === "/") {
-//               pos--;
-//             }
-//             input.value =
-//               input.value.substring(0, pos - 1) +
-//               "_" +
-//               input.value.substring(pos);
-//             input.setSelectionRange(pos - 1, pos - 1);
-//           }
-//         }
-//       });
-
-//       input.addEventListener("input", function (event) {
-//         console.log(`Pressed key: ${event.key}`);
-//         const value = event.target.value.replace(/_/g, "").replace(/\//g, "");
-//         let formattedValue = "__/__/____".split("");
-//         console.log(`Formatted value: ${formattedValue}`);
-
-//         for (
-//           let i = 0, j = 0;
-//           i < value.length && j < formattedValue.length;
-//           i++, j++
-//         ) {
-//           if (j === 2 || j === 5) {
-//             j++;
-//           }
-//           formattedValue[j] = value[i];
-//         }
-
-//         event.target.value = formattedValue.join("");
-//       });
-//     }
-//   });
-// });
-
-// Reusable example
-// document.addEventListener("DOMContentLoaded", function () {
-//   const inputElements = document.querySelectorAll("[fs-input-mask]");
-
-//   inputElements.forEach(function (input) {
-//     const maskType = input.getAttribute("fs-input-mask");
-
-//     if (maskType === "date") {
-//       input.value = "__/__/____";
-
-//       input.addEventListener("keydown", function (event) {
-//         if (event.key === "Backspace" || event.key === "Delete") {
-//           event.preventDefault();
-
-//           let pos = input.selectionStart;
-//           if (pos !== 0) {
-//             if (input.value[pos - 1] === "/") {
-//               pos--;
-//             }
-//             input.value =
-//               input.value.substring(0, pos - 1) +
-//               "_" +
-//               input.value.substring(pos);
-//             input.setSelectionRange(pos - 1, pos - 1);
-//           }
-//         }
-//       });
-
-//       input.addEventListener("input", function (event) {
-//         const value = event.target.value.replace(/_/g, "").replace(/\//g, "");
-//         let formattedValue = "__/__/____".split("");
-
-//         // Extract day, month, and year
-//         let day = value.substring(0, 2);
-//         let month = value.substring(2, 4);
-//         let year = value.substring(4);
-
-//         // Apply default values if needed
-//         day = Math.min(31, parseInt(day, 10) || 31)
-//           .toString()
-//           .padStart(2, "0");
-//         month = Math.min(12, parseInt(month, 10) || 12)
-//           .toString()
-//           .padStart(2, "0");
-//         year = Math.max(
-//           1900,
-//           Math.min(
-//             new Date().getFullYear(),
-//             parseInt(year, 10) || new Date().getFullYear()
-//           )
-//         )
-//           .toString()
-//           .padStart(4, "0");
-
-//         formattedValue[0] = day.charAt(0);
-//         formattedValue[1] = day.charAt(1);
-//         formattedValue[3] = month.charAt(0);
-//         formattedValue[4] = month.charAt(1);
-//         formattedValue[6] = year.charAt(0);
-//         formattedValue[7] = year.charAt(1);
-//         formattedValue[8] = year.charAt(2);
-//         formattedValue[9] = year.charAt(3);
-
-//         for (let i = 0; i < formattedValue.length; i++) {
-//           if (formattedValue[i] !== "_") {
-//             formattedValue[i] = event.target.value[i];
-//           }
-//         }
-
-//         event.target.value = formattedValue.join("");
-//       });
-//     }
-//   });
-// });
-
-// Reusable example
-// document.addEventListener("DOMContentLoaded", function () {
-//   const inputElements = document.querySelectorAll("[fs-input-mask]");
-
-//   inputElements.forEach(function (input) {
-//     const maskType = input.getAttribute("fs-input-mask");
-
-//     if (maskType === "date") {
-//       input.value = "__/__/____";
-
-//       input.addEventListener("keydown", function (event) {
-//         if (event.key === "Backspace" || event.key === "Delete") {
-//           event.preventDefault();
-
-//           let pos = input.selectionStart;
-
-//           if (pos > 0 && pos < 11) {
-//             if (input.value.charAt(pos - 1) !== "/") {
-//               input.value =
-//                 input.value.substring(0, pos - 1) +
-//                 "_" +
-//                 input.value.substring(pos);
-//               input.setSelectionRange(pos - 1, pos - 1);
-//             }
-//           }
-//         }
-//       });
-
-//       input.addEventListener("input", function (event) {
-//         let pos = input.selectionStart;
-//         let newValue = input.value.replace(/[^0-9\/]/g, "_").substr(0, 10);
-//         if (newValue.charAt(pos - 1) === "/") {
-//           pos++;
-//         }
-//         input.value = newValue;
-//         input.setSelectionRange(pos, pos);
-//       });
-//     }
-
-//     for (let i = 0; i < input.value.length; i++) {
-//       console.log(input.value.charAt(i));
-//     }
-//   });
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const inputElements = document.querySelectorAll("[fs-input-mask]");
-
-//   inputElements.forEach(function (input) {
-//     const maskType = input.getAttribute("fs-input-mask");
-
-//     if (maskType === "date") {
-//       input.value = "__/__/____";
-
-//       input.addEventListener("keydown", function (event) {
-//         if (event.key === "Backspace" || event.key === "Delete") {
-//           event.preventDefault();
-
-//           let pos = input.selectionStart;
-//           console.log(pos);
-//           if (pos !== 0) {
-//             let newValue = input.value.split("");
-//             pos--;
-
-//             if (input.value[pos] !== "/") {
-//               newValue[pos] = "_";
-//               input.value = newValue.join("");
-//             } else {
-//               pos--; // Skip the slash '/'
-//             }
-
-//             input.setSelectionRange(pos, pos);
-//           }
-//         }
-//       });
-
-//       input.addEventListener("input", function (event) {
-//         const value = event.target.value.replace(/_/g, "").replace(/\//g, "");
-//         let formattedValue = "__/__/____".split("");
-
-//         for (
-//           let i = 0, j = 0;
-//           i < value.length && j < formattedValue.length;
-//           i++, j++
-//         ) {
-//           if (j === 2 || j === 5) {
-//             j++;
-//           }
-//           formattedValue[j] = value[i];
-//         }
-
-//         event.target.value = formattedValue.join("");
-//       });
-//     }
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   const inputElements = document.querySelectorAll("[fs-input-mask]");
 
   inputElements.forEach(function (input) {
     const maskType = input.getAttribute("fs-input-mask");
-    const maskChar = input.getAttribute("fs-input-mask-char");
+    let maskChar = input.getAttribute("fs-input-mask-char");
 
     if (maskType === "date") {
-      // input.value = "__/__/____";
+      if (maskChar === "") {
+        maskChar = "_";
+      }
+
       input.value = maskChar.repeat(10);
       input.value = input.value
         .split("")
@@ -248,14 +28,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
           let pos = input.selectionStart;
           let newValue = input.value.split("");
-          console.log(newValue);
 
-          if (event.key === "Backspace" || event.key === "Delete") {
-            if (pos !== 0 && input.value[pos - 1] !== "/") {
-              // newValue[pos - 1] = "_";
-              newValue[pos - 1] = maskChar;
-              pos--;
-            }
+          if (pos !== 0 && input.value[pos - 1] !== "/") {
+            newValue[pos - 1] = maskChar;
+            pos--;
           }
 
           input.value = newValue.join("");
@@ -264,14 +40,10 @@ document.addEventListener("DOMContentLoaded", function () {
       });
 
       input.addEventListener("input", function (event) {
-        // const value = event.target.value.replace(/_/g, "").replace(/\//g, "");
         const value = event.target.value
-          // .replace(/_/g, "")
-          // .replace(new RegExp(maskChar === "." ? "\\" : maskChar, "g"), "")
           .replace(new RegExp("\\" + maskChar, "g"), "")
           .replace(/\//g, "")
           .replace(/\D/g, "");
-        // let formattedValue = "__/__/____".split("");
 
         let formattedValue = maskChar.repeat(10).split("");
 
--- js/product-key.js
+++ js/product-key.js
@@ -3,18 +3,96 @@ document.addEventListener("DOMContentLoaded", function () {
 
   inputElements.forEach(function (input) {
     const maskType = input.getAttribute("fs-input-mask");
+    let maskChar = input.getAttribute("fs-input-mask-char");
 
-    input.addEventListener("input", function (event) {
-      let value = event.target.value;
-
-      if (maskType === "productKey") {
-        const formattedValue = value.replace(/[^a-zA-Z0-9]/g, "");
-
-        // Split the formatted value into groups of 5 characters each
-        const groups = formattedValue.match(/.{1,5}/g);
-
-        this.value = groups ? groups.join("-") : "";
+    if (maskType === "productKey") {
+      if (maskChar === "") {
+        maskChar = "_";
       }
-    });
+
+      input.value = maskChar.repeat(29);
+
+      input.value = input.value
+        .split("")
+        .map((char, index) => {
+          if (index === 5 || index === 11 || index === 17 || index === 23) {
+            return "-";
+          } else {
+            return char;
+          }
+        })
+        .join("");
+
+      input.addEventListener("keydown", function (event) {
+        if (event.key === "Backspace" || event.key === "Delete") {
+          event.preventDefault();
+
+          let pos = input.selectionStart;
+          let newValue = input.value.split("");
+          console.log(newValue);
+
+          if (pos !== 0 && input.value[pos - 1] !== "-") {
+            newValue[pos - 1] = maskChar;
+            pos--;
+          }
+
+          input.value = newValue.join("");
+          input.setSelectionRange(pos, pos);
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        const value = event.target.value
+          .replace(new RegExp("\\" + maskChar, "g"), "")
+          .replace(/[^a-zA-Z0-9]/g, "");
+
+        let formattedValue = maskChar.repeat(29).split("");
+
+        formattedValue[5] = "-";
+        formattedValue[11] = "-";
+        formattedValue[17] = "-";
+        formattedValue[23] = "-";
+
+        for (
+          let i = 0, j = 0;
+          i < value.length && j < formattedValue.length;
+          j++
+        ) {
+          if (j === 5 || j === 11 || j === 17 || j === 23) {
+            // formattedValue[j] = "-";
+            continue;
+          } else {
+            formattedValue[j] = value[i++];
+          }
+        }
+
+        // formattedValue[0] = "-";
+        // formattedValue[1] = "-";
+        // formattedValue[-1] = "-";
+        // formattedValue[-1] = "-";
+
+        // console.log(formattedValue[23]);
+
+        // console.log(input.value);
+
+        // for (
+        //   let i = 0, j = 0;
+        //   i < value.length && j < formattedValue.length;
+        //   j++
+        // ) {
+        //   if (j === 5 || j === 11 || j === 17 || j === 23) {
+        //     // Maintain the initial slashes
+        //     formattedValue[j] = "-";
+        //   } else {
+        //     formattedValue[j] = value[i++];
+        //   }
+        // }
+
+        input.value = formattedValue.join("").slice(0, 29);
+
+        console.log(event);
+        console.log(`This is the value from input - ${event.target.value}`);
+      });
+    }
   });
 });
