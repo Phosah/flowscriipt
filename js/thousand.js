2d821cc588886a5dc2201df098fd271ca3eeeeaf
Phosah <efosa.uyi@gmail.com>
2023-11-17 13:47:50 +0100

    update to thousand input mask

--- js/thousand.js
+++ js/thousand.js
@@ -4,17 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
   inputElements.forEach(function (input) {
     const maskType = input.getAttribute("fs-input-mask");
 
-    input.addEventListener("input", function (event) {
-      let value = event.target.value;
+    if (maskType === "thousand") {
+      input.value = "0";
 
-      if (maskType === "thousand") {
+      input.addEventListener("keydown", function (event) {
+        if (this.value === "0" && /\d/.test(event.key)) {
+          event.preventDefault();
+          this.value = event.key;
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        let value = event.target.value;
+
         const formattedValue = value.replace(/[^0-9]/g, "");
 
+        if (formattedValue === "") {
+          this.value = "0";
+          return;
+        }
+
         // Split the formatted value into groups of 3 digits each from the end
-        const groups = formattedValue.match(/\d{1,3}(?=(\d{3})*$)/g);
+        // const groups = formattedValue.match(/\d{1,3}(?=(\d{3})*$)/g);
+        const groups = formattedValue.match(/\d{1,3}(?=(\d{3})*(\D|$))/g);
 
         this.value = groups ? groups.join(",") : "";
-      }
-    });
+      });
+    }
   });
 });
