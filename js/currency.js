18e53863aa392c05e039ec7c4ad9f627168a2627
Phosah <efosa.uyi@gmail.com>
2023-11-17 15:35:08 +0100

    update to currency input, add all scripts to the index.js as a single file

--- js/currency.js
+++ js/currency.js
@@ -3,32 +3,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
   inputElements.forEach(function (input) {
     const maskType = input.getAttribute("fs-input-mask");
+    let maskChar = input.getAttribute("fs-input-currency");
 
-    input.addEventListener("input", function (event) {
-      let value = event.target.value;
+    if (maskType === "currency") {
+      input.value = "0.00";
 
-      if (maskType === "currency") {
+      input.addEventListener("keydown", function (event) {
+        // If the initial value is '0.00' and the entered key is a digit, replace the '0' with the entered digit
+        if (this.value === "0.00" && /\d/.test(event.key)) {
+          event.preventDefault();
+          // this.value = event.key + ".00";
+          this.value = `${maskChar}${event.key}.00`;
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        let value = event.target.value;
+
         let numericValue = value.replace(/[^\d.]/g, "");
-
-        if (numericValue === "" || numericValue === "$ ") {
-          this.value = "";
+        if (numericValue === "" || numericValue === maskChar) {
+          this.value = maskChar + "0.00";
           return;
         }
-
-        if (numericValue.charAt(0) !== "$") numericValue = `$ ${numericValue}`;
+        if (numericValue.charAt(0) !== maskChar && numericValue !== maskChar)
+          numericValue = `${maskChar} ${numericValue}`;
 
         // Ensure only one decimal point is present
         const parts = numericValue.split(".");
         if (parts.length > 2) {
           parts.pop();
         }
-
         if (parts[1] && parts[1].length > 2) {
           parts[1] = parts[1].substring(0, 2);
         }
 
-        this.value = parts.join(".");
-      }
-    });
+        // Add commas as thousand separators to the part before the decimal point
+        const groups = parts[0].match(/\d{1,3}(?=(\d{3})*(\D|$))/g);
+        parts[0] = groups ? groups.join(",") : "";
+
+        // Append the currency symbol to the numeric value
+        this.value = maskChar + parts.join(".");
+      });
+    }
   });
 });
---
+++ js/index.js
@@ -0,0 +1,568 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const inputElements = document.querySelectorAll("[fs-input-mask]");
+
+  inputElements.forEach(function (input) {
+    const maskType = input.getAttribute("fs-input-mask");
+    let maskChar = input.getAttribute("fs-input-mask-char");
+    let maskCurrency = input.getAttribute("fs-input-currency");
+
+    // Date Input mask
+
+    if (maskType === "date") {
+      if (maskChar === "") {
+        maskChar = "_";
+      }
+
+      input.value = maskChar.repeat(10);
+      input.value = input.value
+        .split("")
+        .map((char, index) => {
+          if (index === 2 || index === 5) {
+            return "/";
+          } else {
+            return char;
+          }
+        })
+        .join("");
+
+      input.addEventListener("keydown", function (event) {
+        if (event.key === "Backspace" || event.key === "Delete") {
+          event.preventDefault();
+
+          let pos = input.selectionStart;
+          let newValue = input.value.split("");
+
+          if (pos !== 0 && input.value[pos - 1] !== "/") {
+            newValue[pos - 1] = maskChar;
+            pos--;
+          }
+
+          input.value = newValue.join("");
+          input.setSelectionRange(pos, pos);
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        const value = event.target.value
+          .replace(new RegExp("\\" + maskChar, "g"), "")
+          .replace(/\//g, "")
+          .replace(/\D/g, "");
+
+        let formattedValue = maskChar.repeat(10).split("");
+
+        formattedValue[2] = "/";
+        formattedValue[5] = "/";
+
+        input.value = formattedValue.join("");
+
+        for (
+          let i = 0, j = 0;
+          i < value.length && j < formattedValue.length;
+          j++
+        ) {
+          if (j === 2 || j === 5) {
+            // Maintain the initial slashes
+            formattedValue[j] = "/";
+          } else {
+            formattedValue[j] = value[i++];
+          }
+        }
+
+        input.value = formattedValue.join("").slice(0, 10);
+
+        let day = formattedValue.slice(0, 2).join("");
+        let month = formattedValue.slice(3, 5).join("");
+        let year = formattedValue.slice(6).join("");
+
+        // Day formatting
+        if (parseInt(day.charAt(0)) > 3) {
+          day = "3" + day.charAt(1);
+          formattedValue[0] = day.charAt(0);
+        } else if (
+          parseInt(day.charAt(0)) === 3 &&
+          parseInt(day.charAt(1)) > 1
+        ) {
+          day = day.charAt(0) + "1";
+          formattedValue[1] = day.charAt(1);
+          console.log(`${day.charAt(1)} is greater than 1`);
+        } else {
+          formattedValue[0] = day.charAt(0);
+          formattedValue[1] = day.charAt(1);
+        }
+
+        // Month formatting
+        console.log(`month - ${month}`);
+        let newMonth = month;
+        if (parseInt(month.charAt(0)) > 1) {
+          newMonth = "0" + month.charAt(1);
+          formattedValue[3] = newMonth.charAt(0);
+        } else if (
+          parseInt(month.charAt(0)) === 1 &&
+          parseInt(month.charAt(1)) > 2
+        ) {
+          newMonth = month.charAt(0) + "2";
+          formattedValue[4] = newMonth.charAt(1);
+        } else {
+          formattedValue[3] = month.charAt(0);
+          formattedValue[4] = month.charAt(1);
+        }
+
+        // Day limit based on month
+        if (
+          newMonth === "04" ||
+          newMonth === "06" ||
+          newMonth === "09" ||
+          newMonth === "11"
+        ) {
+          if (parseInt(day) > 30) {
+            day = "30";
+            formattedValue[0] = day.charAt(0);
+            formattedValue[1] = day.charAt(1);
+          }
+        } else if (newMonth === "02") {
+          if (parseInt(day) > 29) {
+            day = "29";
+            formattedValue[0] = day.charAt(0);
+            formattedValue[1] = day.charAt(1);
+          }
+        } else {
+          if (parseInt(day) > 31) {
+            day = "31";
+            formattedValue[0] = day.charAt(0);
+            formattedValue[1] = day.charAt(1);
+          }
+        }
+
+        // Year formatting
+        let newYear = year;
+        if (!isNaN(parseInt(newYear.charAt(3)))) {
+          if (parseInt(newYear) < 1900 || parseInt(newYear) > 2100) {
+            newYear = new Date().getFullYear().toString();
+            formattedValue[6] = newYear.charAt(0);
+            formattedValue[7] = newYear.charAt(1);
+            formattedValue[8] = newYear.charAt(2);
+            formattedValue[9] = newYear.charAt(3);
+          } else {
+            formattedValue[6] = newYear.charAt(0);
+            formattedValue[7] = newYear.charAt(1);
+            formattedValue[8] = newYear.charAt(2);
+            formattedValue[9] = newYear.charAt(3);
+          }
+        }
+
+        event.target.value = formattedValue.join("");
+
+        let nextUnderscoreIndex = input.value.indexOf(maskChar);
+        if (nextUnderscoreIndex !== -1) {
+          input.setSelectionRange(nextUnderscoreIndex, nextUnderscoreIndex);
+        }
+        console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+      });
+    }
+
+    // Product Key input mask
+    if (maskType === "productKey") {
+      if (maskChar === "") {
+        maskChar = "_";
+      }
+
+      input.value = maskChar.repeat(29);
+
+      input.value = input.value
+        .split("")
+        .map((char, index) => {
+          if (index === 5 || index === 11 || index === 17 || index === 23) {
+            return "-";
+          } else {
+            return char;
+          }
+        })
+        .join("");
+
+      input.addEventListener("keydown", function (event) {
+        if (event.key === "Backspace" || event.key === "Delete") {
+          event.preventDefault();
+
+          let pos = input.selectionStart;
+          let newValue = input.value.split("");
+          console.log(newValue);
+
+          if (pos !== 0 && input.value[pos - 1] !== "-") {
+            newValue[pos - 1] = maskChar;
+            pos--;
+          }
+
+          input.value = newValue.join("");
+          input.setSelectionRange(pos, pos);
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        const value = event.target.value
+          .replace(new RegExp("\\" + maskChar, "g"), "")
+          .replace(/[^a-zA-Z0-9]/g, "");
+
+        let formattedValue = maskChar.repeat(29).split("");
+
+        formattedValue[5] = "-";
+        formattedValue[11] = "-";
+        formattedValue[17] = "-";
+        formattedValue[23] = "-";
+
+        for (
+          let i = 0, j = 0;
+          i < value.length && j < formattedValue.length;
+          j++
+        ) {
+          if (j === 5 || j === 11 || j === 17 || j === 23) {
+            // formattedValue[j] = "-";
+            continue;
+          } else {
+            formattedValue[j] = value[i++];
+          }
+        }
+
+        input.value = formattedValue.join("").slice(0, 29);
+
+        // Find the position of the next underscore and set the cursor position to that index
+        let nextUnderscoreIndex = input.value.indexOf(maskChar);
+        if (nextUnderscoreIndex !== -1) {
+          input.setSelectionRange(nextUnderscoreIndex, nextUnderscoreIndex);
+        }
+
+        console.log(event);
+        console.log(`This is the value from input - ${event.target.value}`);
+      });
+    }
+
+    // IPv4 input mask
+    if (maskType === "IPv4") {
+      if (maskChar === "" || maskChar === ".") {
+        maskChar = "_";
+      }
+
+      input.value = maskChar.repeat(15);
+
+      input.value = input.value
+        .split("")
+        .map((char, index) => {
+          if (index === 3 || index === 7 || index === 11) {
+            return ".";
+          } else {
+            return char;
+          }
+        })
+        .join("");
+      console.log(input.value);
+
+      input.addEventListener("keydown", function (event) {
+        if (event.key === "Backspace" || event.key === "Delete") {
+          event.preventDefault();
+
+          let pos = input.selectionStart;
+          let newValue = input.value.split("");
+          console.log(newValue);
+
+          if (pos !== 0 && input.value[pos - 1] !== ".") {
+            newValue[pos - 1] = maskChar;
+            pos--;
+          }
+
+          input.value = newValue.join("");
+          input.setSelectionRange(pos, pos);
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        // const value = event.target.value.replace(/[^0-9.]/g, "");
+        const value = event.target.value.replace(/\D/g, "");
+
+        let formattedValue = maskChar.repeat(15).split("");
+
+        formattedValue[3] = ".";
+        formattedValue[7] = ".";
+        formattedValue[11] = ".";
+
+        for (
+          let i = 0, j = 0;
+          i < value.length && j < formattedValue.length;
+          j++
+        ) {
+          if (j === 3 || j === 7 || j === 11) {
+            formattedValue[j] = ".";
+            // continue;
+          } else {
+            formattedValue[j] = value[i++];
+          }
+        }
+
+        input.value = formattedValue.join("").slice(0, 15);
+
+        let nextUnderscoreIndex = input.value.indexOf(maskChar);
+        if (nextUnderscoreIndex !== -1) {
+          input.setSelectionRange(nextUnderscoreIndex, nextUnderscoreIndex);
+        }
+
+        console.log(event);
+        console.log(`This is the value from input - ${event.target.value}`);
+      });
+
+      // input.addEventListener("input", function (event) {
+      //   let value = event.target.value;
+
+      //   // Remove all non-digit and non-dot characters
+      //   value = value.replace(/[^0-9.]/g, "");
+
+      //   // Remove existing dots
+      //   value = value.replace(/\./g, "");
+
+      //   const segments = value.match(/\d{1,3}/g) || [];
+
+      //   if (segments.length > 4) {
+      //     segments.length = 4;
+      //   }
+
+      //   value = segments.join(".");
+
+      //   event.target.value = value;
+      // });
+    }
+
+    // IPv6 input mask
+    if (maskType === "IPv6") {
+      if (maskChar === "" || maskChar === ":") {
+        maskChar = "_";
+      }
+
+      input.value = maskChar.repeat(39);
+
+      input.value = input.value
+        .split("")
+        .map((char, index) => {
+          if (
+            index === 4 ||
+            index === 9 ||
+            index === 14 ||
+            index === 19 ||
+            index === 24 ||
+            index === 29 ||
+            index === 34
+          ) {
+            return ":";
+          } else {
+            return char;
+          }
+        })
+        .join("");
+      console.log(input.value);
+
+      input.addEventListener("keydown", function (event) {
+        if (event.key === "Backspace" || event.key === "Delete") {
+          event.preventDefault();
+
+          let pos = input.selectionStart;
+          let newValue = input.value.split("");
+          console.log(newValue);
+
+          if (pos !== 0 && input.value[pos - 1] !== ":") {
+            newValue[pos - 1] = maskChar;
+            pos--;
+          }
+
+          input.value = newValue.join("");
+          input.setSelectionRange(pos, pos);
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        const value = event.target.value.replace(/[^0-9a-f]/g, "");
+
+        let formattedValue = maskChar.repeat(39).split("");
+
+        formattedValue[4] = ":";
+        formattedValue[9] = ":";
+        formattedValue[14] = ":";
+        formattedValue[19] = ":";
+        formattedValue[24] = ":";
+        formattedValue[29] = ":";
+        formattedValue[34] = ":";
+
+        for (
+          let i = 0, j = 0;
+          i < value.length && j < formattedValue.length;
+          j++
+        ) {
+          if (
+            j === 4 ||
+            j === 9 ||
+            j === 14 ||
+            j === 19 ||
+            j === 24 ||
+            j === 29 ||
+            j === 34
+          ) {
+            formattedValue[j] = ":";
+            // continue;
+          } else {
+            formattedValue[j] = value[i++];
+          }
+        }
+
+        input.value = formattedValue.join("").slice(0, 39);
+
+        let nextUnderscoreIndex = input.value.indexOf(maskChar);
+        if (nextUnderscoreIndex !== -1) {
+          input.setSelectionRange(nextUnderscoreIndex, nextUnderscoreIndex);
+        }
+
+        console.log(event);
+        console.log(`This is the value from input - ${event.target.value}`);
+      });
+
+      // input.addEventListener("input", function (event) {
+      //   let value = event.target.value;
+
+      //   if (maskType === "IPv6") {
+      //     // Remove all non-hexadecimal and non-colon characters
+      //     value = value.replace(/[^0-9a-fA-F:]/g, "");
+
+      //     // Remove all colons
+      //     value = value.replace(/:/g, "");
+
+      //     // Split the value into segments of 4 characters
+      //     const segments = value.match(/.{1,4}/g) || [];
+
+      //     // If there are more than 8 segments, remove the extra
+      //     if (segments.length > 8) {
+      //       segments.length = 8;
+      //     }
+
+      //     // Join the segments with colons
+      //     value = segments.join(":");
+
+      //     if (value.length > 45) {
+      //       value = value.slice(0, 45);
+      //     }
+      //   }
+
+      //   event.target.value = value;
+      // });
+    }
+
+    // Thousand input mask
+    if (maskType === "thousand") {
+      input.value = "0";
+
+      input.addEventListener("keydown", function (event) {
+        if (this.value === "0" && /\d/.test(event.key)) {
+          event.preventDefault();
+          this.value = event.key;
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        let value = event.target.value;
+
+        const formattedValue = value.replace(/[^0-9]/g, "");
+
+        if (formattedValue === "") {
+          this.value = "0";
+          return;
+        }
+
+        // Split the formatted value into groups of 3 digits each from the end
+        // const groups = formattedValue.match(/\d{1,3}(?=(\d{3})*$)/g);
+        const groups = formattedValue.match(/\d{1,3}(?=(\d{3})*(\D|$))/g);
+
+        this.value = groups ? groups.join(",") : "";
+      });
+    }
+
+    // ************** Decimal input mask ************** //
+    if (maskType === "decimal") {
+      // Set default value to '0'
+      input.value = "0";
+
+      input.addEventListener("keydown", function (event) {
+        // If the initial value is '0' and the entered key is a digit, replace the initial '0' with the entered digit
+        if (this.value === "0" && /\d/.test(event.key)) {
+          event.preventDefault();
+          this.value = event.key;
+        }
+
+        // If a dot already exists in the value, prevent the input of another dot
+        if (event.key === "." && this.value.includes(".")) {
+          event.preventDefault();
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        let value = event.target.value;
+
+        // If the input is empty, set it back to '0'
+        if (value === "") {
+          this.value = "0";
+          return;
+        }
+
+        // Replace non-digit and non-dot characters
+        const numericValue = value.replace(/[^\d.]/g, "");
+
+        // Ensure only one dot is present, and it must be after at least one number
+        const parts = numericValue.split(".");
+        if (parts.length > 1) {
+          const integerPart = parts[0];
+          const decimalPart = parts.slice(1).join("");
+          this.value = /^\d+$/.test(integerPart) ? parts.join(".") : "0";
+        } else {
+          this.value = /^\d+$/.test(parts[0]) ? parts[0] : "0";
+        }
+      });
+    }
+
+    // ************** Currency input mask ************** //
+    if (maskType === "currency") {
+      input.value = "0.00";
+
+      input.addEventListener("keydown", function (event) {
+        // If the initial value is '0.00' and the entered key is a digit, replace the '0' with the entered digit
+        if (this.value === "0.00" && /\d/.test(event.key)) {
+          event.preventDefault();
+          // this.value = event.key + ".00";
+          this.value = `${maskCurrency}${event.key}.00`;
+        }
+      });
+
+      input.addEventListener("input", function (event) {
+        let value = event.target.value;
+
+        let numericValue = value.replace(/[^\d.]/g, "");
+        if (numericValue === "" || numericValue === maskCurrency) {
+          this.value = maskCurrency + "0.00";
+          return;
+        }
+        if (
+          numericValue.charAt(0) !== maskCurrency &&
+          numericValue !== maskCurrency
+        )
+          numericValue = `${maskCurrency} ${numericValue}`;
+
+        // Ensure only one decimal point is present
+        const parts = numericValue.split(".");
+        if (parts.length > 2) {
+          parts.pop();
+        }
+        if (parts[1] && parts[1].length > 2) {
+          parts[1] = parts[1].substring(0, 2);
+        }
+
+        // Add commas as thousand separators to the part before the decimal point
+        const groups = parts[0].match(/\d{1,3}(?=(\d{3})*(\D|$))/g);
+        parts[0] = groups ? groups.join(",") : "";
+
+        // Append the currency symbol to the numeric value
+        this.value = maskCurrency + parts.join(".");
+      });
+    }
+  });
+});
